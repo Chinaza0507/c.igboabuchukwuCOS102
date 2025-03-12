681a2561339070482203f29714574541f7f70b61
Karen IGBOABUCHUKWU <karen@Karens-MacBook-Pro.local>
2025-03-12 08:14:17 +0100

    Week_1 practices/projects

--- week_1/project_1.py
+++
@@ -1,9 +0,0 @@
-P = int(input("Enter the value of (p):"))
-R = int(input("Enter the value of (R):"))
-T = int(input("Enter the value of (T):"))
-
-#calculate the Amount f;r simple interest 
-A = P *(1 + (R/100) * T)
-print("The value of A is {}".format(A))
-
-
